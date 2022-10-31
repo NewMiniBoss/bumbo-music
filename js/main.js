3498feacc7857e19dfc56024f3f951daeef0da8e
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-10-31 08:53:57 -0300

    listas array js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-let pom = document.querySelector('.tecla_pom');
+// modulo 1 e 2 -> pegando o elemento com DOM e fazendo função para ativar o som
 
-function tocaPom() { 
+/* 
+let pom = document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
+}
+*/
+
+// modulo 3 ->
+
+let teclas = document.querySelectorAll('.tecla').length;
+let som = teclas.onclick = tocaSom;
+
+function tocaSom() {
+    document.querySelectorAll('#som').length.play();
 }
